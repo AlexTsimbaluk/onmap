1c7bf78fbc0ee1d9491d4e4c4d04e72d71130e2f
Alexandr Tsimbaluk <aleksandr.tsimbaluk@gmail.com>
2019-10-23 12:11:54 +0300

    google maps api

---
+++ src/Map.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+const handleApiLoaded = (map, maps) => {
+  console.log(map);
+  console.log(maps);
+};
+ 
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 55.633837,
+      lng: 37.588993
+    },
+    zoom: 15
+  };
+
+
+ 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCalL3qcZmf7yCDDV9iYeAck0nKgJUKFm0' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+
+export default Map;
